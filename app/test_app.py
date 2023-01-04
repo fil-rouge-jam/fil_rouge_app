84656411f85a6e725f6b9298409a18f767f45d1c
abdel <abdelhart@hotmail.fr>
2023-01-04 16:54:18 +0100

    FRDAJM-10 fix linter

--- app/test_app.py
+++ app/test_app.py
@@ -19,6 +19,6 @@ def test_hello_route():
     Hello method at / API endpoint to greet user and return number of visit
     """
     response = app.test_client().get('/')
- 
+
     assert response.status_code == 200
     assert response.data.decode('utf-8') == 'Hello'
